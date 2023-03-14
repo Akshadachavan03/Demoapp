2764d4842b3743a6a9dab0ea83e24ed59a02ba56
Akshadachavan03 <127718687+Akshadachavan03@users.noreply.github.com>
2023-03-14 17:08:41 +0530

    Add files via upload

---
+++ examserver.zip_expanded/examserver/src/main/java/com/exam/exam/model/User.java
@@ -0,0 +1,57 @@
+package com.exam.exam.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String username;
+	private String firstname;
+	
+	
+	public User() {
+	
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	
+
+}
