21ccbe526e8009cda534acaaeb1e5e0272038f89
Yeshwant Prabhu <yeshwant.prabhu@atos.net>
2023-03-30 23:17:37 +0530

    added User table

---
+++ SpringBoot_Hibernate_Example (new).zip_expanded/SpringBoot_Hibernate_Example/src/main/java/com/onlinetutorialspoint/model/User.java
@@ -0,0 +1,75 @@
+package com.onlinetutorialspoint.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name="FirstName")
+	private String firstName;
+	
+	@Column(name="LastName")
+	private String lastName;
+	
+	@Column
+	private Long mobileNo;
+	
+	@Column
+	private String password;
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public Long getMobileNo() {
+		return mobileNo;
+	}
+
+
+	public void setMobileNo(Long mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public User() {
+		super();
+	}
+	
+}
+
